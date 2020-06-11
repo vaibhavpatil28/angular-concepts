f44248f3478154f6295cddce55fb6e348bbbc874
Vaibhav Patil <developer.vaibhavpatil@gmail.com>
2020-06-11 12:23:23 +0530

    Angular sanitize all html binding variable before it binds into your html view.For avoiding XSS attacks.

--- src/app/observable/pages/observable.page.ts
+++ src/app/observable/pages/observable.page.ts
@@ -1,13 +1,18 @@
-import{Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { CommunicatorService } from '../communicator.service';
 
 @Component({
-  selector:'observable-page',
-  template:`
+  selector: 'observable-page',
+  template: `
   <sibling-one></sibling-one>
   <sibling-two></sibling-two>
+  <h3>Angular sanitize all data before binding to view</h3>
+        {{htmlSnippet}}
   `,
-  styles:[],
-  providers:[CommunicatorService]
+  styles: [],
+  providers: [CommunicatorService]
 })
-export class ObservablePage{}
+export class ObservablePage {
+
+  htmlSnippet = 'Template <script>alert("0wned")</script> <b>Syntax</b>';
+}
